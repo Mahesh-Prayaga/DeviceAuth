3fa1d030320b6604af832d9d885340564c1197bf
Mahesh Prayaga <maheshprayaga135@gmail.com>
2025-08-17 16:09:28 +0530

    Update server.js and db.js for MongoDB Atlas and Render port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -40,6 +42,6 @@ app.post("/verifyDevice", (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("✅ Server running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
